787fa67b7da268d9136b2c928a27d2bef8ffa4d3
artemohii <ohiiartem@gmail.com>
2023-01-05 18:22:42 +0100

    fix

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ function carouselFn(){
     if ($(window).width() < 500){
        $(".owl-carousel").owlCarousel({
    
-        items:1.5,
+        items:1,
         margin:10,
         loop:true,
         });
+     }else if ($(window).width() < 992){
+        $(".owl-carousel").owlCarousel({
+   
+            items:1.5,
+            margin:10,
+            loop:true,
+            });
+
      }
      else{
       $(".owl-carousel").owlCarousel({
