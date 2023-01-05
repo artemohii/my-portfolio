7c7373d17646222aa2bfaf4a977485bf5351a4f2
artemohii <ohiiartem@gmail.com>
2023-01-05 17:37:28 +0100

    add carousel

---
+++ index.js
@@ -0,0 +1,25 @@
+function carouselFn(){
+    if ($(window).width() < 500){
+       $(".owl-carousel").owlCarousel({
+   
+        items:1.5,
+        margin:10,
+        loop:true,
+        });
+     }
+     else{
+      $(".owl-carousel").owlCarousel({
+   
+       items:3.5,
+       margin:20,
+       loop:true,
+      });
+     }
+    };
+   
+    $(document).ready(function() {
+        carouselFn();
+   });
+   $(window).resize(function(){
+        carouselFn();     
+    });
