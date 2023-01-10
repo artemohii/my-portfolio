19c644268458a50fad489465a71291b2adaf0b45
artemohii <ohiiartem@gmail.com>
2023-01-10 10:36:19 +0100

    fix js

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-function carouselFn(){
+/* function carouselFn(){
     if ($(window).width() < 500){
        $(".owl-carousel").owlCarousel({
    
         items:1,
+        lazyLoad:true,
         margin:10,
         loop:true,
         });
-     }/* else if ($(window).width() < 992){
+     }else if ($(window).width() < 992){
         $(".owl-carousel").owlCarousel({
    
             items:1.5,
+            lazyLoad:true,
             margin:10,
             loop:true,
             });
 
-     } */
+     }
      else{
       $(".owl-carousel").owlCarousel({
    
        items:3.5,
-       margin:20,
+       lazyLoad:true,
+       margin:10,
        loop:true,
       });
      }
@@ -30,4 +33,10 @@ function carouselFn(){
    });
    $(window).resize(function(){
         carouselFn();     
+    }); */
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        lazyLoad:true,
+        loop:true,
+        margin:10
     });
